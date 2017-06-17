2709028fdfa19086e8918eb6d96286cc7a0fce6f
Wojciech Adaszyński <wojciech.adaszynski@gmail.com>
2017-06-17 19:44:51 +0200

    NODE_ENV=development set for npm install

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -24,7 +24,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'git@github.com:wojciechAdaszynski/ocr-selection-app.git',
       path: '/opt/ocr-app',
-      'post-deploy' : 'npm install && NODE_ENV=production npm run build'
+      'post-deploy' : 'NODE_ENV=development npm install && NODE_ENV=production npm run build'
     }
   }
 };
